3c844c9c60ffcf37e80194c231e7cf7276c87a7a
ddesvillechabrol <d.desvillechabrol@gmail.com>
2023-04-20 17:52:53 +0200

    use poetry

--- sequana_pipelines/lora/__init__.py
+++ sequana_pipelines/lora/__init__.py
@@ -1,5 +1,5 @@
 import pkg_resources
 
-version = pkg_resources.require("sequana_lora")[0].version
+version = pkg_resources.require("lora")[0].version
 
 from .src import create_reports, BLAST_KEY, exceptions
